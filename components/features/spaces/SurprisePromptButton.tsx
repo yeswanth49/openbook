1ae4cd05ea683684e8059daaca73ac41e1910e64
yeswanth49 <sinchan123v@gmail.com>
2025-11-07 12:12:30 +0530

    feat: added ! in surprise button

--- components/features/spaces/SurprisePromptButton.tsx
+++ components/features/spaces/SurprisePromptButton.tsx
@@ -25,7 +25,7 @@ export const SurprisePromptButton: React.FC<SurprisePromptButtonProps> = ({ onPr
             onClick={handleClick}
         >
             <Sparkles className="h-4 w-4 sm:h-5 sm:w-5 text-purple-500 dark:text-purple-400 group-hover:rotate-12 transition-transform" />
-            <span className="text-xs sm:text-sm text-neutral-700 dark:text-neutral-300 font-medium">Surprise me!</span>
+            <span className="text-xs sm:text-sm text-neutral-700 dark:text-neutral-300 font-medium">Surprise me!!</span>
         </Button>
     );
 };
